e0280ce7acd5bf3182b835f0a4b09467134a76d7
AndrewSams <stevensams99@yahoo.com>
2025-03-23 23:11:54 +0100

    feat: landing page and error page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,34 @@
-import styled from 'styled-components'
-
-const Wrapper = styled.div``
+import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import { Logo } from '../components'
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <div className='content'>some content</div>
-    </div>
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby lyft crucifix post-ironic, twee yr gastropub letterpress
+            deep v jawn tote bag mustache disrupt blackbird spyplane aesthetic.
+            Pitchfork hot chicken banh mi blog sus snackwave +1 taxidermy put a
+            bird on it solarpunk franzen.
+          </p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn login-link'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
   )
 }
 
