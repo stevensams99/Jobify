ff57dfb3de70908cf97151763cfc50a206eabff9
AndrewSams <stevensams99@yahoo.com>
2025-03-24 00:05:51 +0100

    feat: sidebar and links

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,5 +1,26 @@
+import NavLinks from './NavLinks'
+import Logo from '../components/Logo'
+import Wrapper from '../assets/wrappers/BigSidebar'
+import { useDashboardContext } from '../pages/DashboardLayout'
+
 const BigSidebar = () => {
-  return <div></div>
+  const { showSidebar } = useDashboardContext()
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default BigSidebar
