6239d0c76c5de9701f2ef005331a9db44b24e9dd
AndrewSams <stevensams99@yahoo.com>
2025-03-23 22:41:00 +0100

    feat:routers and initial page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+const Landing = () => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <div className='content'>some content</div>
+    </div>
+  )
+}
+
+export default Landing
