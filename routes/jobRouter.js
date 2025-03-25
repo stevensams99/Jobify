38e0f5bdef7de13b6b0ac16e69cff72f82f09588
AndrewSams <stevensams99@yahoo.com>
2025-03-25 14:44:19 +0100

    feat: add controller and routes

---
+++ routes/jobRouter.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+const router = Router()
+
+import {
+  getAllJobs,
+  createJob,
+  getJob,
+  updateJob,
+  deleteJob,
+} from '../controllers/jobController.js'
+
+router.route('/').get(getAllJobs).post(createJob)
+
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
+
+export default router
