75a64213a57050b6ac9e52cecaea3ad319f0267f
AndrewSams <stevensams99@yahoo.com>
2025-03-25 15:52:02 +0100

    feat: add mongodb connection, controller using mongoose, create a JobModel for the MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+import 'express-async-errors'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 const app = express()
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 
 //routers
 import jobRouter from './routes/jobRouter.js'
@@ -36,6 +38,12 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5100
 
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}...`)
-})
+try {
+  await mongoose.connect(process.env.MONGO_URL)
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`)
+  })
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
