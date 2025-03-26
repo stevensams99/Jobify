6e1cc25063953828af9e129f61d64813eb6b0b04
AndrewSams <stevensams99@yahoo.com>
2025-03-26 17:17:04 +0100

    feat:add user model, that have 3 method, get current user, update user, and application stats. setup user verivication only admin can access application stats

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,10 @@
-import { UnauthenticatedError } from '../errors/customErrors.js'
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from '../errors/customErrors.js'
 import { verifyJWT } from '../utils/tokenUtils.js'
 
-export const authenticateUser = async (req, res, next) => {
+export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies
   if (!token) throw new UnauthenticatedError('authentication invalid')
   try {
@@ -10,5 +13,14 @@ export const authenticateUser = async (req, res, next) => {
     next()
   } catch (error) {
     throw new UnauthenticatedError('authentication invalid')
+  }
+}
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError('Unauthorized to access this route')
+    }
+    next()
   }
 }
