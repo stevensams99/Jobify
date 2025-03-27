c2833bd2b4ad364345d1a7ea4f239f41ae1e0f2c
AndrewSams <stevensams99@yahoo.com>
2025-03-27 09:32:40 +0100

    feat:send request to register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,28 @@
 import { FormRow, Logo } from '../components'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
-import { Link } from 'react-router-dom'
+import { Form, redirect, useNavigation, Link } from 'react-router-dom'
+import customFetch from '../utils/customFetch'
+import { toast } from 'react-toastify'
 
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  console.log(data)
+  try {
+    await customFetch.post('/auth/register', data)
+    toast.success('Registration Successful')
+    return redirect('/login')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
 const Register = () => {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
   return (
     <Wrapper>
-      <form className='form'>
+      <Form method='post' className='form'>
         <Logo />
         <h4>Register</h4>
         <FormRow type='text' name='name' defaultValue='john' />
@@ -18,8 +35,8 @@ const Register = () => {
         <FormRow type='text' name='location' defaultValue='german' />
         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret123' />
-        <button type='submit' className='btn btn-block'>
-          submit
+        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>
           Already a member?
@@ -27,7 +44,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   )
 }
