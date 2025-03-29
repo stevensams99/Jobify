f0d34c47bd7d433bf7d4af8fe3aa312348738e26
AndrewSams <stevensams99@yahoo.com>
2025-03-29 21:26:51 +0100

    feat:admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { action as deleteJobAction } from './pages/DeleteJob'
 import { loader as editJobLoader } from './pages/EditJob'
 import { loader as dashboardLoader } from './pages/DashboardLayout'
 import { loader as allJobsLoader } from './pages/AllJobs'
+import { loader as appStatLoader } from './pages/Admin'
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
@@ -77,6 +78,7 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+            loader: appStatLoader,
           },
           {
             path: 'edit-job/:id',
