27dfcb9c80cca26b5f9f3278eb7364f3fa01a4bb
AndrewSams <stevensams99@yahoo.com>
2025-03-29 23:14:12 +0100

    fix: add submit button to component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import { Link, Form, redirect, useNavigation } from 'react-router-dom'
+import { Link, Form, redirect } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
-import { FormRow, Logo } from '../components'
+import { FormRow, Logo, SubmitBtn } from '../components'
 import customFetch from '../utils/customFetch'
 import { toast } from 'react-toastify'
 
@@ -18,8 +18,6 @@ export const action = async ({ request }) => {
 }
 
 const Login = () => {
-  const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'
   return (
     <Wrapper>
       <Form method='post' className='form'>
@@ -27,9 +25,7 @@ const Login = () => {
         <h4>Login</h4>
         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret123' />
-        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
-          {isSubmitting ? 'submitting...' : 'submit'}
-        </button>
+        <SubmitBtn />
         <button type='button' className='btn btn-block'>
           explore the app
         </button>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
-import { FormRow, Logo } from '../components'
+import { FormRow, Logo, SubmitBtn } from '../components'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
-import { Form, redirect, useNavigation, Link } from 'react-router-dom'
+import { Form, redirect, Link } from 'react-router-dom'
 import customFetch from '../utils/customFetch'
 import { toast } from 'react-toastify'
 
@@ -18,8 +18,6 @@ export const action = async ({ request }) => {
   }
 }
 const Register = () => {
-  const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'
   return (
     <Wrapper>
       <Form method='post' className='form'>
@@ -35,9 +33,7 @@ const Register = () => {
         <FormRow type='text' name='location' defaultValue='german' />
         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret123' />
-        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
-          {isSubmitting ? 'submitting...' : 'submit'}
-        </button>
+        <SubmitBtn />
         <p>
           Already a member?
           <Link to='/login' className='member-btn'>
