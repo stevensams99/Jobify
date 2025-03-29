6a07c17e0760c1ca28a0ac05f23352812e0cb514
AndrewSams <stevensams99@yahoo.com>
2025-03-29 21:36:30 +0100

    feat: add avatar in user schema and url retrival function in server.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -17,6 +17,8 @@ const UserSchema = new mongoose.Schema({
     enum: ['user', 'admin'],
     default: 'user',
   },
+  avatar: String,
+  AvatarPublicId: String,
 })
 
 UserSchema.methods.toJSON = function () {
