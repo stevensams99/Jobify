20f707fff657bd147577a182e5dc79fbf1738e37
AndrewSams <stevensams99@yahoo.com>
2025-03-31 01:22:12 +0200

    fix: update user middleware, set up command production in package.json, use frontend with express

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 app.use(cookieParser())
 app.use(express.json())
 
@@ -44,6 +44,9 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter)
 app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+})
 //NOT FOUND
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' })
