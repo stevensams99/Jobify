a1ca9f7baf19d6905d0006b93efe5271ef346f81
AndrewSams <stevensams99@yahoo.com>
2025-03-31 01:45:41 +0200

    set back express static to /client/dist

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
 app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 })
 //NOT FOUND
 app.use('*', (req, res) => {
