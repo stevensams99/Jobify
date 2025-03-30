8fcb04dda516ae72201d3d246c144b9680c0e17f
AndrewSams <stevensams99@yahoo.com>
2025-03-31 00:49:14 +0200

    fix: erase defaultValue on register and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -37,8 +37,8 @@ const Login = () => {
       <Form method='post' className='form'>
         <Logo />
         <h4>Login</h4>
-        <FormRow type='email' name='email' defaultValue='john@gmail.com' />
-        <FormRow type='password' name='password' defaultValue='secret123' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
         <SubmitBtn />
         <button type='button' className='btn btn-block' onClick={loginDemoUser}>
           explore the app
