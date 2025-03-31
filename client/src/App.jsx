50d93dd3e6909434123ea9fc2bc22ba1d2fc06d3
AndrewSams <stevensams99@yahoo.com>
2025-03-31 16:22:14 +0200

    feat: loading component, set up react query in client/app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
   HomeLayout,
   Landing,
@@ -34,6 +36,13 @@ export const checkDefaultTheme = () => {
 
 checkDefaultTheme()
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 const router = createBrowserRouter([
   {
     path: '/',
@@ -101,6 +110,11 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 export default App
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,6 +1,12 @@
-import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom'
+import {
+  Outlet,
+  redirect,
+  useLoaderData,
+  useNavigate,
+  useNavigation,
+} from 'react-router-dom'
 import Wrapper from '../assets/wrappers/Dashboard'
-import { Navbar, BigSidebar, SmallSidebar } from '../components'
+import { Navbar, BigSidebar, SmallSidebar, Loading } from '../components'
 import { useState, createContext, useContext } from 'react'
 import { checkDefaultTheme } from '../App'
 import customFetch from '../utils/customFetch'
@@ -21,6 +27,8 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
   // temp
   const { user } = useLoaderData()
   const navigate = useNavigate() //same as redirect
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === 'loading'
   const [showSidebar, setShowSidebar] = useState(false)
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme())
 
@@ -57,7 +65,7 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
           <div>
             <Navbar />
             <div className='dashboard-page'>
-              <Outlet context={{ user }} />
+              {isPageLoading ? <Loading /> : <Outlet context={{ user }} />}
             </div>
           </div>
         </main>
