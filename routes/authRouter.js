955e4a049df8ae5879c74640c4190fd4af914072
AndrewSams <stevensams99@yahoo.com>
2025-03-31 19:08:04 +0200

    feat: react query implementation on delete job, edit job, add job, all jobs, profile update, login, get current user, applied api limiter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,8 +5,15 @@ import {
   validateRegisterInput,
   validateLoginInput,
 } from '../middleware/validationMiddleware.js'
+import rateLimiter from 'express-rate-limit'
 
-router.route('/register').post(validateRegisterInput, register)
-router.route('/login').post(validateLoginInput, login)
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15,
+  message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
+})
+
+router.route('/register').post(apiLimiter, validateRegisterInput, register)
+router.route('/login').post(apiLimiter, validateLoginInput, login)
 router.route('/logout').get(logout)
 export default router
